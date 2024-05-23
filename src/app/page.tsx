84d3a465c80b48c510c9f040006f0e2d75772f4f
Loai <43700105+Loai106@users.noreply.github.com>
2024-05-24 00:54:15 +0300

    creating the post list at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from '@nextui-org/react'
+import PostList from '@/components/posts/PostList'
+import { Button ,User ,Link ,Divider} from '@nextui-org/react'
 import React from 'react'
+import { AiOutlineHeart ,AiOutlineComment,AiOutlineShareAlt } from 'react-icons/ai'
 
 export default function page() {
   return (
-    <div>Home</div>
+    <div className='my-4 flex justify-center  px-4'>
+        <div className='w-full max-w-screen-md'>
+          <div className='flex flex-col'>
+          <PostList/>
+          </div>
+       </div>
+     </div>
+  
+       
   )
 }
   
